4beef7372a4afb483a19df52a7f1569919f87fc1
Prakhar Srivastava <sriprakhar2@gmail.com>
2024-06-22 13:17:33 +0530

    graphql setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
 import express from "express";
 
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+
 const startServer = async () => {
   const app = express();
   const PORT = Number(process.env.PORT) || 8000;
+
+  app.use(express.json());
+
+  const server = new ApolloServer({
+    typeDefs: `
+        type Query {
+            hello: String
+            say(name: String): String
+        }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "Hello",
+        say: (parent, { name }) => `Hello ${name}`,
+      },
+    },
+  });
+
+  await server.start();
+
+  app.use("/graphql", expressMiddleware(server));
 
   app.get("/", (req, res) => res.json({ message: "Server is running" }));
 
