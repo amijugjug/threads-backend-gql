d9895929d107e90e63fe5c643cd9b196d1f86fff
Prakhar Srivastava <sriprakhar2@gmail.com>
2024-06-22 15:28:42 +0530

    chore: Prisma setup for postgres and mutation example

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "./db";
 
 const startServer = async () => {
   const app = express();
@@ -15,17 +16,48 @@ const startServer = async () => {
             hello: String
             say(name: String): String
         }
+
+        type Mutation {
+            createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+        }
     `,
     resolvers: {
       Query: {
         hello: () => "Hello",
         say: (parent, { name }) => `Hello ${name}`,
       },
+      Mutation: {
+        createUser: async (
+          parent,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              firstName,
+              lastName,
+              email,
+              password,
+              salt: "",
+            },
+          });
+
+          return true;
+        },
+      },
     },
   });
 
   await server.start();
-
   app.use("/graphql", expressMiddleware(server));
 
   app.get("/", (req, res) => res.json({ message: "Server is running" }));
